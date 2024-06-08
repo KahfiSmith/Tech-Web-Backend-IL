4a5bfc8564a213eca66ec499f319e7b9e0ce476c
KahfiSmith <alkahfii2018@gmail.com>
2024-06-08 23:06:38 +0700

    final

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { Connection } from "./config/db.js";
 import dotenv from "dotenv";
+import notesRouter from "./routes/note.route.js";
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   return res.status(200).json("Server is running");
 });
+
+app.use('/', notesRouter);
 
 app.listen(process.env.APP_PORT, () => {
   Connection();
